76af77d3c0776fabdb44ab75c57f584aba9e0cbb
DannyMtwenty <dannywanyoike@gmail.com>
2019-09-22 12:17:38 +0300

    updated diseases and reminder

---
+++ app/src/main/java/com/dan/M_chick/DrinkingWater.java
@@ -0,0 +1,13 @@
+package com.dan.M_chick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkingWater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinking_water);
+    }
+}
---
+++ app/src/main/java/com/dan/M_chick/InjectionActivity.java
@@ -0,0 +1,13 @@
+package com.dan.M_chick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InjectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_injection);
+    }
+}
